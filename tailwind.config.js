f1184b007675be21bf2f7c43da16b28216fff4f3
Rajat Choudhary <143991789+rajat89882@users.noreply.github.com>
2024-12-01 20:02:25 +0530

    add

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#14035F",
+        secondary: "#7385CB",
+        yellow: "#FBF891",
+        primaryBG: "#EBEDF8",
+      },
+    },
+  },
+  plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+        placeholder: {
+          color: "#000",
+          opacity: "0.5",
+
+        },
+        ".placeholder-primary": {
+          color: "#14035F",
+          opacity: "0.5",
+        },
+      };
+      addUtilities(newUtilities, ["responsive", "hover"]);
+    },
+  ],
+};
